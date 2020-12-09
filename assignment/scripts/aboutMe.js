c910047b4e672e71ebe9a5046af5e54f251be314
Josh Gulledge <joshcalebgulledge@gmail.com>
2020-12-08 19:15:44 -0600

    This is the first push to git

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,54 +1,108 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
+const firstName = `Josh`;
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+const lastName = `Gulledge`;
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
+const fullName = firstName + " " + lastName;
 
 // 4 - Console log the value of `fullName`
+console.log(fullName);
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
+let luckyNumber = 89;
 
-// 6 - Console log this sentence, adding in the variables you created above: 
+// 6 - Console log this sentence, adding in the variables you created above:
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
+// console.log("My name is " + fullName + ", and I think " + luckyNumber + " is a winner!");
+console.log(`My name is ${fullName}, and I think ${luckyNumber} is a winner!`);
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
+let adventurous = true;
 
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
+let food = "thin crust pizza";
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
+let pets = 0;
 
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
+let friendsPets = 3;
 
 // 11 - Add two pets to your `pets` variable
+pets += 2;
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
+const allowedPets = 0;
 
-// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
+// 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
-
+if (adventurous) {
+  console.log("Adventures are great!");
+} else {
+  console.log("How about we stay home?");
+};
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
+if (luckyNumber === 2 && adventurous) {
+  console.log("Roll the dice!");
+} else if (luckyNumber === 89 && adventurous) {
+  console.log("Lets make some memories!");
+} else {
+  console.log("Staying home is nice too. Lets find a new game.");
+}
 
-// 15 - Write a conditional that console logs "I can have more pets!" 
+// 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
+if (pets < allowedPets) {
+  console.log("I can have more pets!");
+} else if (allowedPets === pets) {
+  console.log("I have enough pets");
+} else {
+  console.log("Oh no, I have too many pets!");
+}
 
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
 // correctly checks the `pets` and `friendsPets` variables, and
 // assigns the highest value to `mostPets`. There's several possibilities --
-// be sure to think through all the scenarios. 
+// be sure to think through all the scenarios.
 // console.log `mostPets` after the conditional has run.
+let mostPets = 0;
+if (pets > friendsPets) {
+  mostPets = pets;
+} else  {
+  mostPets = friendsPets;
+};
+console.log(mostPets);
 
 // 17 - Rewrite question 16 with a `switch` statement. You'll need to do research!
+let morePets = 0;
+
+switch (true) {
+  case (friendsPets > pets):
+    console.log("Friend has more pets");
+    morePets = friendsPets;
+    break;
+  case (pets > friendsPets):
+    console.log("I have more pets");
+    morePets = pets;
+  break;
+  default:
+    console.log("We have the same amount of pets");
+    morePets = pets;
+};
+console.log(morePets);
+
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-
-
+ adventurous ? console.log("Adventures are great!") : console.log("How about we stay home?");
--- assignment/scripts/describe.js
+++ assignment/scripts/describe.js
@@ -1,6 +1,6 @@
 // WHAT IS THIS EVEN DOING?
 
-// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening. 
+// For questions 1-3, each code block is syntactically and logically correct. Your job is to describe what is happening.
 // They are in isolation -- variables in one question do not affect other questions.
 // Describe the code as it would be run, and make sure you give us the console.log's value at the end.
 // (Don't just say 'we console log the variable' -- instead say something like 'we console log 'yes'')
@@ -26,9 +26,9 @@ if (number >= 2) {
 
 
 // 1. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
+// a vairable is created that holds the name "Dane"
+// the name variable is checked against the string "Mary", since "Dane" is not
+// equal to "Mary" the else block runs and we console log "How do you do?"
 //
 
 //CODE
@@ -45,10 +45,12 @@ if (name === 'Mary') {
 */
 
 // 2. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+//we create a variable named secret but dont give it a value, then a variable named
+//code with a value of 123. We check to see if code is equal to 123, since it is
+//code block runs and the value of "super" is assigned to secret, then code is mutliplied
+//by two. We then check to see if code is greater than 250, it now has a value of 246
+// which is less than 250 so that block does not run. We then log secret to the Console
+// and that logs the string "super".
 
 //CODE
 /*
@@ -70,10 +72,15 @@ console.log(secret)
 */
 
 // 3. WRITE YOUR DESCRIPTION HERE
-// 
-// 
-// 
-//
+//here we start with three variables, one boolean set to true and two number variables.
+//The first line of the if statement checks to see if the studend value is set to true,
+// and that the zip is greater than 80000. Although the student variable is true the zip
+// is less than 80000 so this line does not run, the next line checks if student is false
+// or the age is less than 30. student is true and age is greater than 30 so neither of
+// these conditions are met and the line does not run. Next is a line that only checks to
+//see if the student variable is true, and it is, so this line runs which logs to the Console
+// the string "Welcome to Prime!". Now the code exits this code block.
+// And thank you for the welcome :)
 
 //CODE
 /*
@@ -97,8 +104,8 @@ if (isStudent === true && zip > 80000 ) {
 
 
 // REVERSE
-// Now, for questions 4-6, we'll give you a description and some code. 
-// Make the code match the description-- comment above the problem code and a potential solution. 
+// Now, for questions 4-6, we'll give you a description and some code.
+// Make the code match the description-- comment above the problem code and a potential solution.
 // Look closely -- things are logically incorrect or missing and wrong in the code!
 
 // 0. WRITE YOUR DESCRIPTION HERE
@@ -111,9 +118,9 @@ if (isStudent === true && zip > 80000 ) {
 /*
 let number = 1;
 
-// number-- decrements number, and the instructions ask to increment. 
+// number-- decrements number, and the instructions ask to increment.
 // Should be number++.
-number--; 
+number--;
 
 if (number >= 2) {
   console.log('yes');
@@ -125,7 +132,7 @@ if (number >= 2) {
 
 
 
-//4. We start with three variables -- colorOne is set to 'blue' and colorTwo is set to 'red', and mix is set to true. We check if mix is true 
+//4. We start with three variables -- colorOne is set to 'blue' and colorTwo is set to 'red', and mix is set to true. We check if mix is true
 //-- it is, so we set colorOne and colorTwo to 'purple'
 
 /*
@@ -139,7 +146,7 @@ if (mix === true) {
 */
 
 //5. We start with two variables -- temp is set to 40 and time is set to 4.
-// We check if temp is higher than 39 and if time is greater or equal to  4 
+// We check if temp is higher than 39 and if time is greater or equal to  4
 // -- they are so we console.log 'throw away the food!'
 
 /*
@@ -152,7 +159,7 @@ if (temp > 39 || time >= 4) {
 */
 
 //6. We start with two variables -- age is set to 21 and minAge is set to 21.
-// We check if age is greater than minAge 
+// We check if age is greater than minAge
 // -- they are, so we console.log 'enter'.
 
 /*
@@ -165,4 +172,3 @@ if(minAge <= age) {
   console.log('enter');
 }
 */
-
